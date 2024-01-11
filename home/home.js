798e82ce5dfd4d4e3c38cb32c521e60d51d038f5
poptaro <noahahn199@gmail.com>
2024-01-11 09:04:09 -0600

    second commit with new features

--- home/home.js
+++ home/home.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const listed = document.querySelector(".mainList");
     const inputField = document.querySelector(".inputField");
     const btnDel = document.querySelector('.deleto');
+    let listDel = document.querySelector('.liDel')
 
 
 
@@ -12,24 +13,37 @@ document.addEventListener('DOMContentLoaded', () => {
         if(inputField.value.trim() === ''){
             alert("You need to put SOMETHING IN THE TEXT BOX!!!!");
         } else {
-                const itemId = Date.now();
+            const itemId = Date.now();
             listed.insertAdjacentHTML(
                 'beforeend',
-                `<li id="${itemId}">${inputField.value}</li>`
+                `<li id="${itemId}">${inputField.value}<button class="strike">-</button><button class="liDel">X</button></li>`
             );
         }
-
         inputField.value = '';
     });
 
-
-    console.log(btnDel);
-    btnDel.addEventListener('click', () => {
-        if(listed.getElementsByTagName('li').length == 0){
-            alert("There is nothing to delete!");
-        } else {
-            const lasty = document.querySelector('li:last-child');
-            lasty.remove();
+    document.addEventListener('click', (event) => {
+        if (event.target.classList.contains('liDel')) {
+            const listItem = event.target.closest('li');
+            listItem.remove();
         }
-    })
+        if (event.target.classList.contains('strike')) {
+            const listItem = event.target.closest('li');
+            if(listItem.style.textDecoration == "line-through"){
+                listItem.style.textDecoration = "";
+            } else {
+                listItem.style.textDecoration = "line-through";
+            }
+
+        }
+    });
+
+    // btnDel.addEventListener('click', () => {
+    //     if(listed.getElementsByTagName('li').length == 0){
+    //         alert("There is nothing to delete!");
+    //     } else {
+    //         const lasty = document.querySelector('li:last-child');
+    //         lasty.remove();
+    //     }
+    // })
 });
